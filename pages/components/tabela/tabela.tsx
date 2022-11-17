4d4004bed55864cee1e5303405cb187659aaa16e
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2022-11-17 15:24:21 -0300

    tabela.tsx

--- pages/components/tabela/tabela.tsx
+++ pages/components/tabela/tabela.tsx
@@ -1,41 +1,41 @@
-// import React from "react";
-// import { Tabela, TH, TD, TR } from "../componentes";
+import React from "react";
+() => null;''
+/*import React from "react";
 
-// const TabelaIMC: React.FC = () => {
-//     return (
-//         <Tabela>
-//             <TR>
-//                 <TH>Resultados Correspondentes</TH>
-//                 <TH>Classificação</TH>
-//                 <TH>Obesidade(graus)</TH>
-//             </TR>
-//             <TR>
-//                 <TD>Menor que 18,5</TD>
-//                 <TD>Magreza</TD>
-//                 <TD>0</TD>
-//             </TR>
-//             <TR>
-//                 <TD>Entre 18,5 e 25</TD>
-//                 <TD>Normal</TD>
-//                 <TD>0</TD>
-//             </TR>
-//             <TR>
-//                 <TD>Entre 25 e 30</TD>
-//                 <TD>SobrePeso</TD>
-//                 <TD>I</TD>
-//             </TR>
-//             <TR>
-//                 <TD>Entre 30 e 40</TD>
-//                 <TD>Obesidade</TD>
-//                 <TD>II</TD>
-//             </TR>
-//             <TR>
-//                 <TD>Maior que 40</TD>
-//                 <TD>Obesidade Grave</TD>
-//                 <TD>III</TD>
-//             </TR>
-//         </Tabela>
-//     );
-// };
-
-// export default TabelaIMC;
+const TabelaIMC: React.FC = () => {
+    return (
+        <Tabela>
+            <TR>
+                <TH>Resultados Correspondentes</TH>
+                <TH>Classificação</TH>
+                <TH>Obesidade(graus)</TH>
+            </TR>
+            <TR>
+                <TD>Menor que 18,5</TD>
+                <TD>Magreza</TD>
+                <TD>0</TD>
+            </TR>
+            <TR>
+                <TD>Entre 18,5 e 25</TD>
+                <TD>Normal</TD>
+                <TD>0</TD>
+            </TR>
+            <TR>
+                <TD>Entre 25 e 30</TD>
+                <TD>SobrePeso</TD>
+                <TD>I</TD>
+            </TR>
+            <TR>
+                <TD>Entre 30 e 40</TD>
+                <TD>Obesidade</TD>
+                <TD>II</TD>
+            </TR>
+            <TR>
+                <TD>Maior que 40</TD>
+                <TD>Obesidade Grave</TD>
+                <TD>III</TD>
+            </TR>
+        </Tabela>
+    );
+};export default TabelaIMC;
+*/
